29739f7c081843ef32eeed04e99d58f10233a7ae
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-09-18 10:40:01 -0400

    Ajout de la methode showDateHeader déjà utilisé dans chat-page

--- frontend/src/app/chat/pages/chat-page/chat-affichage/chat-affichage.component.ts
+++ frontend/src/app/chat/pages/chat-page/chat-affichage/chat-affichage.component.ts
@@ -9,5 +9,15 @@ import { Message } from '../../../model/message.model';
   styleUrl: './chat-affichage.component.css'
 })
 export class ChatAffichageComponent {
+   //Recevoir les messages
 @Input() messages: Message[] = [];
+// La méthode showDateHeader de chat-page
+showDateHeader(messages: Message[], i: number): boolean {
+    if (i === 0) {
+      return true;
+    }
+    const prev = new Date(messages[i - 1].timestamp).setHours(0, 0, 0, 0);
+    const curr = new Date(messages[i].timestamp).setHours(0, 0, 0, 0);
+    return prev !== curr;
+  }
 }
