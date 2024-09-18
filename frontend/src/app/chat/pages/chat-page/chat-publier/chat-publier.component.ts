ae70305fdb621ce949b09f73690e76e685dc4026
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-09-18 10:59:06 -0400

    Ajustement de chat publier

--- frontend/src/app/chat/pages/chat-page/chat-publier/chat-publier.component.ts
+++ frontend/src/app/chat/pages/chat-page/chat-publier/chat-publier.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MessagesService } from '../../services/messages.service';
+import { AuthenticationService } from 'src/app/login/services/authentication.service';
 
 @Component({
   selector: 'app-chat-publier',
@@ -8,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrl: './chat-publier.component.css'
 })
 export class ChatPublierComponent {
+   username = this.authenticationService.getUsername();
+   messageForm = this.fb.group({
+    msg: '',
+  });
+constructor(
+    private fb: FormBuilder,
+    private messagesService: MessagesService,
+    private authenticationService: AuthenticationService
+  ) {}
+onPublishMessage() {
+    if (
+      this.username() &&
+      this.messageForm.valid &&
+      this.messageForm.value.msg
+    ) {
+      this.messagesService.postMessage({
+        text: this.messageForm.value.msg,
+        username: this.username()!,
+        timestamp: Date.now(),
+      });
+    }
+    this.messageForm.reset();
+  }
+
+
 
 }
