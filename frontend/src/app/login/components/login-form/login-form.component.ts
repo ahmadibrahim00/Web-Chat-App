952b2ff99caa403d89bfe5e08c4be00793ae7745
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-09-25 20:49:53 -0400

    Mise a jour des import dans login form TS

--- frontend/src/app/login/components/login-form/login-form.component.ts
+++ frontend/src/app/login/components/login-form/login-form.component.ts
@@ -1,13 +1,15 @@
 import { Component, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { UserCredentials } from '../../model/user-credentials';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule,MatInputModule, ReactiveFormsModule],
 })
 export class LoginFormComponent {
   loginForm = this.fb.group({
