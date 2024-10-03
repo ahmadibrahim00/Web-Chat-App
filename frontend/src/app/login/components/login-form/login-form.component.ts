f9c290b048603152ff88e1d5aa3961b976b64eff
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-10-02 21:37:14 -0400

    Mise a jour des imports afin d'integrer angular material

--- frontend/src/app/login/components/login-form/login-form.component.ts
+++ frontend/src/app/login/components/login-form/login-form.component.ts
@@ -1,13 +1,16 @@
 import { Component, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserCredentials } from '../../model/user-credentials';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule,MatInputModule, ReactiveFormsModule],
 })
 export class LoginFormComponent {
   loginForm = this.fb.group({
