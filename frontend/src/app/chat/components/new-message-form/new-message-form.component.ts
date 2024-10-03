c5ca79e726121384b64e65893660553b7213c9f0
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-10-03 12:46:08 -0400

    Importation des modules angular material

--- frontend/src/app/chat/components/new-message-form/new-message-form.component.ts
+++ frontend/src/app/chat/components/new-message-form/new-message-form.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MessagesService } from '../../services/messages.service';
 import { AuthenticationService } from 'src/app/login/services/authentication.service';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-new-message-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatIconModule, MatButtonModule, MatInputModule],
   templateUrl: './new-message-form.component.html',
   styleUrl: './new-message-form.component.css'
 })
