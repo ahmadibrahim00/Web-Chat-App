3d9ad1d3ef3cc03833b57804d50793c8091fff25
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-10-07 22:14:13 -0400

    bootRun

--- frontend/backend/src/main/java/com/inf5190/chat/auth/AuthController.java
+++ frontend/backend/src/main/java/com/inf5190/chat/auth/AuthController.java
@@ -27,8 +27,21 @@ public class AuthController {
 
     @PostMapping(AUTH_LOGIN_PATH)
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        // À faire...
-        return null;
+      String username = loginRequest.username;
+      String password = loginRequest.password;
+      SessionData SD = new SessionData(username);
+      String sessionId = sessionManager.addSession(SD);
+
+      ResponseCookie cookie = ResponseCookie.from("sid", sessionId)
+         .httpOnly(true)
+         .path("/")
+         .maxAge(24 * 60 * 60) // 24 heures en secondes
+         .build();
+
+      LoginResponse loginResponse = new LoginResponse(username);
+      return ResponseEntity.ok()
+         .header(HttpHeaders.SET_COOKIE, cookie.toString())
+         .build();
     }
 
     @PostMapping(AUTH_LOGOUT_PATH)
