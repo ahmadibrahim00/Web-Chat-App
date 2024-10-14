325fdfc596be4034836e3152c8c059ec198b980e
Gabriel Michaud <michaud.gabriel.3@courrier.uqam.ca>
2024-10-14 02:21:33 -0400

    Correction d'import de htppProvider

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
