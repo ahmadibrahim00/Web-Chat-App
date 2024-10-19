163240d6a60df74e49457333a4f05d190c42df57
Gabriel Michaud <michaud.gabriel.3@courrier.uqam.ca>
2024-10-19 00:02:00 -0400

    Ajout dépendances et connection pour firebase

--- backend/src/main/java/com/inf5190/chat/ChatApplication.java
+++ backend/src/main/java/com/inf5190/chat/ChatApplication.java
@@ -1,12 +1,21 @@
 package com.inf5190.chat;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.inf5190.chat.auth.AuthController;
 import com.inf5190.chat.auth.filter.AuthFilter;
 import com.inf5190.chat.auth.session.SessionManager;
@@ -22,8 +31,24 @@ public class ChatApplication {
     @Value("${cors.allowedOrigins}")
     private String allowedOriginsConfig;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
+        try {
+            if (FirebaseApp.getApps().size() == 0) {
+                FileInputStream serviceAccount = new FileInputStream("firebase-key.json");
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                LOGGER.info("Initializing Firebase application.");
+                FirebaseApp.initializeApp(options);
+            }
+            LOGGER.info("Firebase application already initialized.");
+            SpringApplication.run(ChatApplication.class, args);
+        } catch (IOException e) {
+            System.err.println(
+                    "Could not initialise application. Please check youservice account key path");
+        }
     }
 
     /**
