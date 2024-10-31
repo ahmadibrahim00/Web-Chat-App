73f6f74c563273e002eac52de08c2f353c7de85d
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-10-30 23:01:30 -0400

    Création de la clé et manipulation des sessions

--- backend/src/main/java/com/inf5190/chat/auth/session/SessionManager.java
+++ backend/src/main/java/com/inf5190/chat/auth/session/SessionManager.java
@@ -1,24 +1,50 @@
 package com.inf5190.chat.auth.session;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import org.springframework.stereotype.Repository;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.io.Encoders;
+import javax.crypto.SecretKey;
 
-/**
- * Classe qui gère les sessions utilisateur.
- * 
- * Pour le moment, on gère en mémoire.
- */
+import org.springframework.stereotype.Repository;
+import java.util.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+
 @Repository
 public class SessionManager {
 
+    private static final String SECRET_KEY_BASE64;
+    private final SecretKey secretKey;
+    private final JwtParser jwtParser;
     private final Map<String, SessionData> sessions = new HashMap<String, SessionData>();
 
+    static {
+        SecretKey generatedKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        SECRET_KEY_BASE64 = Encoders.BASE64.encode(generatedKey.getEncoded());
+    }
+
+    public SessionManager() {
+       this.secretKey= Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY_BASE64));
+       this.jwtParser= Jwts.parser().verifyWith(this.secretKey).build();
+   }
+
     public String addSession(SessionData authData) {
-        final String sessionId = this.generateSessionId();
-        this.sessions.put(sessionId, authData);
-        return sessionId;
+      Instant now = Instant.now();
+
+        return Jwts.builder()
+            .setSubject(authData.username())            
+            .setAudience("chat-application")                  
+            .setIssuedAt(Date.from(now))                     
+            .setExpiration(Date.from(now.plus(2, ChronoUnit.HOURS)))
+            .signWith(secretKey, SignatureAlgorithm.HS256)  
+            .compact();
     }
 
     public void removeSession(String sessionId) {
@@ -26,10 +52,23 @@ public class SessionManager {
     }
 
     public SessionData getSession(String sessionId) {
-        return this.sessions.get(sessionId);
-    }
+        try {
+
+            String username = jwtParser.parseClaimsJws(sessionId)
+                .getBody()
+                .getSubject(); 
+
+            return new SessionData(username);
+
+        } catch (JwtException e) {
+         
+            return null;    
+      }
+   }
 
     private String generateSessionId() {
         return UUID.randomUUID().toString();
     }
 }
+
+
