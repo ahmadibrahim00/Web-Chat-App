ea5cad58572c931dc2ebd01c53c1437e4acf0e8b
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-11-04 18:34:53 -0500

    Intégration du polling des messages dans le service d'authentification

--- frontend/src/app/login/services/authentication.service.ts
+++ frontend/src/app/login/services/authentication.service.ts
@@ -4,32 +4,38 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { LoginResponse } from '../services/login-response';
 import { firstValueFrom } from 'rxjs';
+import { MessagesService } from "../../chat/services/messages.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   static KEY = 'username';
-
   private username = signal<string | null>(null);
 
-  constructor(private httpClient: HttpClient) {
-    this.username.set(localStorage.getItem(AuthenticationService.KEY));
+  constructor(
+    private httpClient: HttpClient, 
+    private messagesService: MessagesService
+  ) {
+    const savedUsername = localStorage.getItem(AuthenticationService.KEY);
+    if (savedUsername) {
+      this.username.set(savedUsername);
+      this.messagesService.startPolling();
+    }
   }
 
   async login(userCredentials: UserCredentials): Promise<void> {
-    await firstValueFrom(
+    const response = await firstValueFrom(
       this.httpClient.post<LoginResponse>(
         `${environment.backendURL}/auth/login`,
         userCredentials,
-        {
-          withCredentials: true,
-        }
+        { withCredentials: true }
       )
-    ).then((response) => {
-      localStorage.setItem(AuthenticationService.KEY, response.username);
-      this.username.set(response.username);
-    });
+    );
+    
+    localStorage.setItem(AuthenticationService.KEY, response.username);
+    this.username.set(response.username);
+    this.messagesService.startPolling();
   }
 
   async logout(): Promise<void> {
@@ -37,17 +43,17 @@ export class AuthenticationService {
       this.httpClient.post<void>(
         `${environment.backendURL}/auth/logout`,
         {},
-        {
-          withCredentials: true,
-        }
+        { withCredentials: true }
       )
-    ).then(() => {
-      localStorage.removeItem(AuthenticationService.KEY);
-      this.username.set(null);
-    });
+    );
+    
+    localStorage.removeItem(AuthenticationService.KEY);
+    this.username.set(null);
+    this.messagesService.stopPolling();
   }
 
   getUsername(): Signal<string | null> {
     return this.username;
   }
 }
+
