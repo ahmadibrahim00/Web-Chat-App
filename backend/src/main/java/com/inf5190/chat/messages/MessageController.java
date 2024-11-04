ddd3d920178ba3e700e673e9d48cf4f1d214b3a4
AhmadIbrahim00 <ahmadibra@hotmail.ca>
2024-11-04 18:24:39 -0500

    Intégration du WebSocket et amélioration de la gestion des sessions pour la sécurité des messages

--- backend/src/main/java/com/inf5190/chat/messages/MessageController.java
+++ backend/src/main/java/com/inf5190/chat/messages/MessageController.java
@@ -1,6 +1,7 @@
 package com.inf5190.chat.messages;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CookieValue;
 
 import com.inf5190.chat.messages.model.Message;
 import com.inf5190.chat.messages.repository.MessageRepository;
 import com.inf5190.chat.websocket.WebSocketManager;
+import com.inf5190.chat.auth.session.SessionData;
+import com.inf5190.chat.auth.session.SessionManager;
+import com.inf5190.chat.auth.AuthController;
 
 /**
  * Contrôleur qui gère l'API de messages.
@@ -24,25 +29,25 @@ public class MessageController {
 
     private final MessageRepository messageRepository;
     private final WebSocketManager webSocketManager;
+    private final SessionManager sessionManager;
 
     public MessageController(MessageRepository messageRepository,
-            WebSocketManager webSocketManager) {
+            WebSocketManager webSocketManager, SessionManager sessionManager) {
         this.messageRepository = messageRepository;
         this.webSocketManager = webSocketManager;
+        this.sessionManager = sessionManager;
     }
 
     @GetMapping(MESSAGES_PATH)
-    public ResponseEntity<List<Message>> getMessages(@RequestParam Long id) {
+    public ResponseEntity<List<Message>> getMessages(@RequestParam(required = false) String id) 
+        throws InterruptedException, ExecutionException {
         return ResponseEntity.ok().body(messageRepository.getMessages(id));
     }
 
+
     @PostMapping(MESSAGES_PATH)
-    public ResponseEntity<String> createMessage(@RequestBody Message newMessage) {
-        Message message = messageRepository.createMessage(newMessage);
-        if (message != null) {
-            webSocketManager.notifySessions();
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    public Message createMessage(@RequestBody Message newMessage, @CookieValue(AuthController.SESSION_ID_COOKIE_NAME) String sessionId) throws InterruptedException, ExecutionException {
+    SessionData sessionData = this.sessionManager.getSession(sessionId);
+    return this.messageRepository.createMessage(newMessage, sessionData.username());
     }
 }
