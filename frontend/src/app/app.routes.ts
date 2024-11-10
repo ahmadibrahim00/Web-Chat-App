ecfcf7574b83be24e1fe24c65ff3924ea9925cb6
Gabriel Michaud <michaud.gabriel.3@courrier.uqam.ca>
2024-11-10 14:05:41 -0500

    Redirection automatique à la page de login quand pas connecté

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login/pages/login-page/login-page.component';
 import { ChatPageComponent } from './chat/pages/chat-page/chat-page.component';
 import { loginPageGuard } from './login/guards/login-page.guard';
+import { chatPageGuard } from './login/guards/chat-page.guard';
 
 export const routes: Routes = [
-  { path: 'chat', component: ChatPageComponent },
+  { path: 'chat', component: ChatPageComponent, canActivate: [chatPageGuard] },
   { path: '**', component: LoginPageComponent, canActivate: [loginPageGuard] },
 ];
